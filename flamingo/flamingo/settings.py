d2c57aa26eefee44d10c21750b5bbf35e3ac1057
TheRedLady <christina.hristakieva@gmail.com>
2016-08-19 13:36:11 +0300

    Merge branch rolev into chris

--- flamingo/flamingo/settings.py
+++ flamingo/flamingo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
