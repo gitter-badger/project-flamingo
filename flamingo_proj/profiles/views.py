f5cda666c5dd492278d3e3e6d1571b25cba9fc37
TheRedLady <christina.hristakieva@gmail.com>
2016-09-13 11:35:53 +0300

    Add search for users

--- flamingo_proj/profiles/views.py
+++ flamingo_proj/profiles/views.py
@@ -1,8 +1,20 @@
+import operator
+
+
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from django.shortcuts import render
+
 
 from models import Profile
 from posts.models import Post
+from utils import get_query
+
+
+MyUser = get_user_model()
 
 
 class ProfileView(LoginRequiredMixin, generic.DetailView):
@@ -20,3 +32,15 @@ class GoToProfile(LoginRequiredMixin, generic.base.RedirectView):
         kwargs['pk'] = self.request.user.id
         self.url = '/profile/{}/'.format(kwargs['pk'])
         return super(GoToProfile, self).get_redirect_url(*args, **kwargs)
+
+
+@login_required
+def search_users(request):
+    query_string = ''
+    found_users = None
+    if('q' in request.GET) and request.GET['q'].strip():
+        query_string = request.GET['q']
+        user_query = get_query(query_string, ['first_name', 'last_name', ])
+        found_users = MyUser.objects.filter(user_query)
+    return render(request, 'profiles/search.html',
+                  context={'search_results': found_users, 'search': query_string})
