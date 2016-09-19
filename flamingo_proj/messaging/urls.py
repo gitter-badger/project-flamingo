29816c1534eb14d602d72324d0562687a614ceb2
TheRedLady <christina.hristakieva@gmail.com>
2016-09-19 18:37:57 +0300

    Add send message button to profile

--- flamingo_proj/messaging/urls.py
+++ flamingo_proj/messaging/urls.py
@@ -5,10 +5,11 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(permanent=True, url='inbox'), name='messages_redirect'),
+    url(r'^$', RedirectView.as_view(permanent=True, url='inbox'), name='messages-redirect'),
     url(r'^inbox/$', views.inbox, name='inbox'),
-    url(r'^outbox/$', views.outbox, name='outbox'),
+    url(r'^sent/$', views.sent, name='sent'),
     url(r'^compose/$', views.compose, name='compose'),
-    url(r'^view/(?P<message_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<message_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<message_id>[0-9]+)/delete/$', views.delete_message, name='delete'),
     url(r'^trash/$', views.trash, name='trash'),
 ]
