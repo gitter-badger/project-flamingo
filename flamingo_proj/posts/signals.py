777bd23e8db3e2063b02e0650cf47ac39a9d4940
SimeonRolev <simorolev@gmail.com>
2016-09-20 12:50:36 +0300

    requirements

--- flamingo_proj/posts/signals.py
+++ flamingo_proj/posts/signals.py
@@ -8,23 +8,24 @@ from django.urls import reverse
 
 @receiver(post_save, sender=settings.AUTH_POST_MODEL)
 def create_tags(sender, instance, created, **kwargs):
-    old_refs = instance.tag_set.all()
-    hash_tags = instance.get_hash_tags()
-    split_content = instance.split_by_hashtag()
-    for t in hash_tags:
-        # Adding the new tags
-        obj, created = models.Tag.objects.get_or_create(tag=t)
-        obj.posts.add(instance)
-        obj.save()
-        tag_link = reverse('posts:tag', kwargs={'tag': t[1:]})
-        for index, elem in enumerate(split_content):
-            if elem == t:
-                split_content[index] = '<a href="{}">{}</a>'.format(tag_link, t)
-    result_content = ''.join(split_content)
-    this_instance = models.Post.objects.filter(id=instance.id)
-    this_instance.update(content=result_content)
-
-    old_refs = old_refs.exclude(tag__in=hash_tags)
-    for old in old_refs:
-        old.posts.remove(instance)
-        old.save()
+    # old_refs = instance.tag_set.all()
+    # hash_tags = instance.get_hash_tags()
+    # split_content = instance.split_by_hashtag()
+    # for t in hash_tags:
+    #     # Adding the new tags
+    #     obj, created = models.Tag.objects.get_or_create(tag=t)
+    #     obj.posts.add(instance)
+    #     obj.save()
+    #     tag_link = reverse('posts:tag', kwargs={'tag': t[1:]})
+    #     for index, elem in enumerate(split_content):
+    #         if elem == t:
+    #             split_content[index] = '<a href="{}">{}</a>'.format(tag_link, t)
+    # result_content = ''.join(split_content)
+    # this_instance = models.Post.objects.filter(id=instance.id)
+    # this_instance.update(content=result_content)
+    #
+    # old_refs = old_refs.exclude(tag__in=hash_tags)
+    # for old in old_refs:
+    #     old.posts.remove(instance)
+    #     old.save()
+    instance.create_hashtags()
